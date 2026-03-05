1e0a5e15e3c5143056ed524222011fcdd975aca1
tenqjapan <takato03061037@gmail.com>
2026-03-05 21:20:38 +0900

    作業開始APIの実装中

--- backend/models.py
+++ backend/models.py
@@ -11,3 +11,11 @@ class User(Base):
     email = Column(String(100), nullable=False, unique=True)
     password = Column(String(255), nullable=False)
     created_date_time = Column(TIMESTAMP, server_default=text("CURRENT_TIMESTAMP"), nullable=False)
+
+class Time(Base):
+    __tablename__ = "times"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, server_default=text("gen_random_uuid()"))
+    user_id = Column(UUID(as_uuid=True), nullable=False)
+    start_time = Column(TIMESTAMP, nullable=False)
+    end_time = Column(TIMESTAMP, nullable=True)
---
+++ backend/schemas/manageTime.py
@@ -0,0 +1,5 @@
+# from pydantic import BaseModel, EmailStr
+# from uuid import UUID
+# from datetime import datetime
+
+# class 
